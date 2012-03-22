5546014f5f110dbc648ef14dd24c8cfb12735076
Paweł Szymański <out.of.logins.error@gmail.com>
2012-03-22 22:31:16 +0100

    Dodano .gitignore oraz poprawiono kodowanie

---
+++ k/gr.1/Data_dzien.cs
@@ -0,0 +1,54 @@
+using System;
+public class Data_dzien {
+	private int rok;
+	private int miesiac;
+	private int dzien;
+
+	public Data_dzien(int _rok, int _miesiac, int _dzien) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator>(Data_dzien x, Data_dzien y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator<=(Data_dzien x, Data_dzien y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator<(Data_dzien x, Data_dzien y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator>=(Data_dzien x, Data_dzien y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator==(Data_dzien x, Data_dzien y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator!=(Data_dzien x, Data_dzien y) {
+		throw new System.Exception("Not implemented");
+	}
+	public void Rok(int _rok) {
+		throw new System.Exception("Not implemented");
+	}
+	public void Miesiac(int _miesac) {
+		throw new System.Exception("Not implemented");
+	}
+	public void Dzien(int _dzien) {
+		throw new System.Exception("Not implemented");
+	}
+	public int Rok() {
+		throw new System.Exception("Not implemented");
+	}
+	public int Miesiac() {
+		throw new System.Exception("Not implemented");
+	}
+	public int Dzien() {
+		throw new System.Exception("Not implemented");
+	}
+	public override bool Equals(object obj) {
+		throw new System.Exception("Not implemented");
+	}
+	public override int GetHashCode() {
+		throw new System.Exception("Not implemented");
+	}
+
+
+}
---
+++ k/gr.1/Wpis.cs
@@ -0,0 +1,55 @@
+using System;
+public class Wpis {
+	private Data poczatek;
+	private Data koniec;
+	private string tytul;
+
+	public Wpis(Data _poczatek, Data _koniec, String _tytul) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator>(Wpis x, Wpis y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator<=(Wpis x, Wpis y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator<(Wpis x, Wpis y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator>=(Wpis x, Wpis y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator==(Wpis x, Wpis y) {
+		throw new System.Exception("Not implemented");
+	}
+	public static bool operator!=(Wpis x, Wpis y) {
+		throw new System.Exception("Not implemented");
+	}
+	public override bool Equals(object obj) {
+		throw new System.Exception("Not implemented");
+	}
+	public override int GetHashCode() {
+		throw new System.Exception("Not implemented");
+	}
+	public void Poczatek(Data _poczatek) {
+		throw new System.Exception("Not implemented");
+	}
+	public void Koniec(Data _koniec) {
+		throw new System.Exception("Not implemented");
+	}
+	public void Tytul(string _tytul) {
+		throw new System.Exception("Not implemented");
+	}
+	public Data Poczatek() {
+		throw new System.Exception("Not implemented");
+	}
+	public Data Koniec() {
+		throw new System.Exception("Not implemented");
+	}
+	public string Tytul() {
+		throw new System.Exception("Not implemented");
+	}
+
+
+
+}
